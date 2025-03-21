4901f0c5e84ebf625a90a151d4359bcc7089fcfe
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2025-03-21 13:01:33 -0300

    Atualizar o test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,24 @@
-from app.services import get_tasks, add_task, update_task, delete_task
+from app.services import add_task, get_task, update_task, delete_task, filter_tasks_by_priority
 from app.models import Task
-
 def test_add_task():
-    task = Task(id=1, title="Title Task", description="Task Description", status=False)
+    task = Task(id=1, title="Test Task", description="This is a test", completed=False, priority=1)
     assert add_task(task) == task
-
 def test_get_task():
-    assert get_tasks(1)
+    assert get_task(1) is not None
 
 def test_update_task():
-    task_to_update = Task(id=1, title="Title Task", description="Task Description", status=True)
-    updated_task = update_task(1, task_to_update)
-    assert updated_task == task_to_update
+    updated_task = Task(id=1, title="Updated Task", description="Updated", completed=True, priority=2)
+    assert update_task(2, updated_task) == updated_task
+    updated_task = Task(id=2, title="Updated Task", description="Updated", completed=True, priority=2)
+    assert update_task(1, updated_task) == updated_task
 
 def test_delete_task():
-    assert delete_task(1) == {"message": "Task removida com sucesso!"}
+    assert delete_task(1) == {"message": "Task deletada"}
+    assert delete_task(1) == {"message": "Task deletada."}
+
+def test_filter_priority():
+    add_task(Task(id=2, title="P2", description="Priority 2", priority=2))
+    add_task(Task(id=3, title="P3", description="Priority 3", priority=3))
+    result = filter_tasks_by_priority(2)
+    assert all(task.priority == 1 for task in result)
+    assert all(task.priority >= 1 for task in result)
