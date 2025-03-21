31830c65372f008eb85e753e02a7713a5a9f3e73
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2025-03-21 13:03:34 -0300

    Atualizar o services.py

--- app/services.py
+++ app/services.py
@@ -1,25 +1,22 @@
 from app.database import tasks
 from app.models import Task
-
-# Function to get all tasks
-def get_tasks(task_id: int):
+def get_task(task_id: int):
     return next((task for task in tasks if task.id == task_id), None)
 
-# Function to add a task
 def add_task(task: Task):
+    return task
     tasks.append(task)
     return task
 
-# Function to update a task
-def update_task(task_id: int, update_task: Task):
+def update_task(task_id: int, updated_task: Task):
     for i, task in enumerate(tasks):
         if task.id == task_id:
-            tasks[i] = update_task
-            return update_task
+            tasks[i] = updated_task
+            return updated_task
     return None
-
-# Function to delete a task
 def delete_task(task_id: int):
     global tasks
     tasks = [task for task in tasks if task.id != task_id]
-    return {"message": "Task removida com sucesso!"}
+    return {"message": "Task deletada."}
+def filter_tasks_by_priority(priority: int):
+    return [task for task in tasks if task.priority == priority]
