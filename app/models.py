92159d3f3ebf3492039d8c08efee4981a79745a4
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2025-03-21 13:06:21 -0300

    Atualizar o models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
 
-# Class Task is a model for the task object
 class Task(BaseModel):
     id: int
     title: str
     description: str
-    status: bool = False
+    completed: bool = False
+    priority: int
